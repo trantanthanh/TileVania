14c56e87867b2326177c480ea126a168595cb377
trantanthanh <trantanthanh@gmail.com>
2023-08-08 14:27:48 +0700

    player moving, flip sprite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,52 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] float runSpeed = 10.0f;
+    Vector2 playerVelocity = new Vector2();
+
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
+
+    Animator myAnimator;
     // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void Run()
     {
-        myRigidbody.velocity = moveInput;
+        // Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed * Time.deltaTime, myRigidbody.velocity.y);
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1.0f);
+        }
+        myAnimator.SetBool("IsRunning", playerHasHorizontalSpeed);
     }
 }
