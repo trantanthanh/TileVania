6b2812c312c058897ce545cb2ddf0352a18e3601
Thành Trần Tấn <trantanthanh@gmail.com>
2023-08-14 23:00:23 +0700

    add exit port for 3 levels

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -12,7 +12,12 @@ public class LevelExit : MonoBehaviour
     IEnumerator LoatNextLevel()
     {
         yield return new WaitForSecondsRealtime(2.0f);
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
