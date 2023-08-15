9d644ab69c0468547ce9f02637d59cf476b19c8c
Thành Trần Tấn <trantanthanh@gmail.com>
2023-08-15 14:42:08 +0700

    add sfx collect coin

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSFX;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position, 0.5f);
             Destroy(gameObject);
         }
     }
